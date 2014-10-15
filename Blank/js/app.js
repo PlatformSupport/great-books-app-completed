ffc8168fb581411828520e012fe0630eac0a6925
Brandon Satrom <brandon@kendoui.com>
2014-10-15 15:19:00 +0000

    Finish setting up initial version for tutorial

--- Blank/js/app.js
+++ Blank/js/app.js
@@ -3,13 +3,24 @@
 
     // create an object to store the models for each view
     window.Books = {
-      
+      data: new kendo.data.DataSource({
+        transport: {
+          read: {
+              url: "data/books.js",
+              type: "get",
+              dataType: "json"
+          }
+        },
+        schema: {
+            data: "books"
+        }
+      })
     };
 
     document.addEventListener('deviceready', function () {  
       navigator.splashscreen.hide();
-      app = new kendo.mobile.Application(document.body);
-
+      
+      app = new kendo.mobile.Application(document.body, { skin: "flat" });
     }, false);
 
     window.app = app;
