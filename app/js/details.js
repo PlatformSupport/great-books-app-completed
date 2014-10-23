0000db9fadd25f8d9960d19a4c4b2219b44b46c8
Brandon Satrom <bsatrom@gmail.com>
2014-10-23 11:45:21 -0500

    tutorial fixes

--- app/js/details.js
+++ app/js/details.js
@@ -1,7 +1,7 @@
 (function () {
     var currentBook;
     window.BookDetail = {
-      show: function() {
+        show: function() {
         //Pull the ISBN number from the query string
         var location = window.location.toString();
         var isbn = location.substring(location.lastIndexOf('?')+4);
@@ -9,16 +9,16 @@
         // Filter the DataSource bt ISBN to get the selected record
         window.Books.data.filter({ field: "isbn", operator: "eq", value: isbn });
         currentBook = window.Books.data.view()[0];
-       
-      },
-      hide: function() {
+        
+    },
+    hide: function() {
         // When the user navigates away from the page, remove the filter
         window.Books.data.filter([]);
-      },
-      openLink : function() {
+    },
+    openLink : function() {
         // Will use the Cordova InAppBrowser plugin when deployed to a device. Opens a new window in
         // the simulator
         window.open(currentBook.amazon_url, '_blank', 'location=yes');
-      }
-    };
+    }
+};
 }());
